1806c346af631877ea3d1eb855c013cc859c477a
Gopikrishnan Rajeev <gopikrishnan.rmg@live.in>
2024-01-11 21:16:10 -0500

    Added categorized array to prevent duplicate requests

--- frontend/YTLangF/YTLangF.js
+++ frontend/YTLangF/YTLangF.js
@@ -1,14 +1,27 @@
+var categorized = [];
+
 async function parseElements() {
   const content = document.getElementsByClassName(
     "yt-simple-endpoint inline-block style-scope ytd-thumbnail"
   );
+  const content_box = document.getElementsByClassName(
+    "text-wrapper style-scope ytd-video-renderer"
+  );
   for (var i = 0; i < content.length; i++) {
     var url = content[i].getAttribute("href");
+    if (categorized.includes(url)) continue;
+
     if (url !== null && !url.includes("shorts")) {
       var urlReq = "https://youtu.be/" + url.split("=")[1].split("&")[0];
       const response = await fetch("http://localhost:8000/video/" + urlReq);
       const data = await response.json();
       console.log(urlReq.toString() + " " + JSON.stringify(data));
+
+      if (data["status"] === "success") {
+        categorized.push(url);
+        content_box[i].innerText +=
+          "\n" + JSON.stringify(JSON.parse(data["message"])["langs"]);
+      }
     }
   }
 }

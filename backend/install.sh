6fadde9ad1744d5091f34e3cb29cf03fa628e6a7
Gopikrishnan Rajeev <gopikrishnan.rmg@live.in>
2024-01-22 21:45:56 -0500

    Added ipfs to install.sh

--- backend/install.sh
+++ backend/install.sh
@@ -1,2 +1,12 @@
+ipfs_version=v0.26.0
 sudo apt install python3 python3-pip uvicorn ffmpeg
 pip3 install -r requirements.txt
+wget https://dist.ipfs.tech/kubo/$ipfs_version/kubo_$ipfs_version_linux-amd64.tar.gz
+tar -xvzf kubo_v0.26.0_linux-amd64.tar.gz
+cd kubo
+sudo bash install.sh
+ipfs init
+ipfs config Routing.Type dht
+echo "/key/swarm/psk/1.0.0/
+/base16/
+8828b3b593d8b7b37db860a70460bc2fa7539140f45d7ff855b30a6765c8c947" > ~/.ipfs/swarm.key
